09746d175d1b9fc1107d044bbb6d69c55b7855cb
Eduardo Ângelo <eduardoangelo20001@gmail.com>
2024-04-05 21:55:08 -0300

    acertando erro de path de imagens principais

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -1,14 +1,14 @@
 <template>
       <header class="banner">
     <div class="apresentacao">
-      <img src="./imagens/imagens_principais/logo.svg" alt="Logo do Cookin' Up" class="logo">
+      <img src="/./imagens/imagens_principais/logo.svg" alt="Logo do Cookin' Up" class="logo">
 
       <p class="cabecalho-lg frase-cabecalho">
         <span class="texto-verde">Um banquete de ideias para</span> despertar o chef que há em você!
       </p>
       <p class="subtitulo-lg">Explore novas receitas todos os dias com os ingredientes que estão ao seu alcance!</p>
     </div>
-    <img src="./imagens/imagens_principais/foto-banner.png" alt="Foto de uma mulher cozinhando com uma bacia de vidro na mão. Ela está sorrindo e está apoiando um celular entre seu rosto e seu ombro" class="foto-banner">
+    <img src="/./imagens/imagens_principais/foto-banner.png" alt="Foto de uma mulher cozinhando com uma bacia de vidro na mão. Ela está sorrindo e está apoiando um celular entre seu rosto e seu ombro" class="foto-banner">
   </header>
 </template>
 
